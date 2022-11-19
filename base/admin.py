a3b2e572f3d3856bfe2d80227d82b99a8d6effe6
Cheetamaru <vitalvat@gmail.com>
2022-11-19 23:39:14 +0200

    feat: add genre model

--- base/admin.py
+++ base/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import MangaTitle, MangaAuthor
+from .models import MangaTitle, MangaAuthor, Genre
 
 admin.site.register(MangaTitle)
 admin.site.register(MangaAuthor)
+admin.site.register(Genre)
--- base/models.py
+++ base/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
-
 class MangaAuthor(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
@@ -21,6 +25,9 @@ class MangaTitle(models.Model):
     endDate = models.DateField(null=True)
     firstCoverImage = models.ImageField(upload_to='base/uploads/covers', null=True, blank=True)
 
+    authors = models.ManyToManyField(MangaAuthor, related_name="authors", null=True, blank=True)
+    genres = models.ManyToManyField(Genre, related_name="genres", null=True, blank=True)
+
     class Status(models.TextChoices):
         FINISHED = 'finished', _('Finished')
         ONGOING = 'ongoing', _('Publishing')
@@ -29,10 +36,6 @@ class MangaTitle(models.Model):
         NOT_STARTED = 'notstarted', _('Not Yet Published')
 
     status = models.CharField(max_length=12, null=True, choices=Status.choices)
-
-    authors = models.ManyToManyField(MangaAuthor, related_name="authors", null=True, blank=True)
-
-    genres = models.TextField(null=True, blank=True) #TODO: think what to do with it
 
     def __str__(self):
         return self.name
